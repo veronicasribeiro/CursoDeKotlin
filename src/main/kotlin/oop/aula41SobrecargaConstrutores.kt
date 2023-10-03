0217e25812dfb59377168fef53a97ec75db05da9
Verônica Ribeiro <127154698+veronicasribeiro@users.noreply.github.com>
2023-10-02 22:41:02 -0300

    Adicionado aula 41 sobre sobrecarga de construtores

---
+++ src/main/kotlin/oop/aula41SobrecargaConstrutores.kt
@@ -0,0 +1,30 @@
+package oop
+
+// SOBRECARGA DE CONSTRUTOR
+fun main() {
+    val user1 = User("veronicaRibeiro", true)
+    println(user1.nome)
+    println(user1.isAdmin)
+
+    val user2 = User("reidospiratas")
+    println(user2.nome)
+    println(user2.isAdmin)
+}
+
+// 1. Usando valor default
+//class User (var nome: String, var isAdmin: Boolean = false) { }
+
+// 2. Usando um segundo construtor dentro da classe
+
+// O user1 chama uma construtor com uma String e um Boolean (logo abaixo)
+class User (var nome: String, var isAdmin: Boolean) {
+
+    // O user2 só chamou o nome, como ele passou só uma String ele chamou o construtor 2 (logo abaixo)
+    // Como ele chamou o contrutor abaixo eu preciso passar o valor do isAdmin (Boolean)
+    // o this executa para o construtor principal
+    // this(nome, false) - foi pego o nome e jogado para o principal, logo após passou automáticamente um falso
+    // diretamente para o construtor principal
+    constructor(nome: String) : this(nome, false) {
+        println("Executei o construtor 2")
+    }
+}
