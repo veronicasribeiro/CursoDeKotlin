2f1c1f89b8db8f816a3c7de0bdb782acd5a776ad
Verônica Ribeiro <127154698+veronicasribeiro@users.noreply.github.com>
2023-10-13 22:13:15 -0300

    Recapitulando tipos de classes

---
+++ src/main/kotlin/oop/aula54RecapitulandoTiposClasses.kt
@@ -0,0 +1,7 @@
+package oop
+
+// 1. Classes Aninhadas (Nested Class) - Uma classe dentro da outra
+// 2. Classes Internas (Inner Class) - Como Nested Class mas você terá acesso as propriedades da classe relacionada
+// 3. Classes Enumeradas (Enum Class) - Criar coisas que são "tagueadas" ou coisas que tem uma certa propriedade para
+// usar em uma certa quatidade
+
