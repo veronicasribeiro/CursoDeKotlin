930009069b48fba43fbbe75b7ccd07e576aabe80
Verônica Ribeiro <127154698+veronicasribeiro@users.noreply.github.com>
2023-10-21 12:23:07 -0300

    Copiando Objetos no Kotlin

---
+++ src/main/kotlin/oop/aula59CopiandoObjetosNoKotlin.kt
@@ -0,0 +1,28 @@
+package oop
+
+import condicoesLogicas.CartaoDeCredito
+
+fun main() {
+    val cartao1 = CartaoCredito(5982549, "05/24")
+    val cartao2 = CartaoCredito(8594495, "01/26")
+    val usuarioA = UsuarioCopiado("Zoro", false, cartao1)
+    // Copiando usuario A para o usuario B
+    //val usuarioB = usuarioA.copy()
+    //println(usuarioB)
+
+    // Posso modificar prorpriedades da cópia
+    val usuarioB = usuarioA.copy(isAdmin = true)
+    println(usuarioB)
+    println("_______________")
+
+    // Utilidade aplicada
+    // O mesmo usuário criou um segundo cartão
+
+    // Foi aproveitado as propriedades passadas mas as propriedades do cartão foram alteradas
+    val novoUsuarioA = usuarioA.copy(cartaoCredito = cartao2)
+    println(novoUsuarioA)
+
+}
+
+data class CartaoCredito(var numero: Int, var vencimento: String)
+data class UsuarioCopiado(var nome: String, var isAdmin: Boolean, var cartaoCredito: CartaoCredito)
