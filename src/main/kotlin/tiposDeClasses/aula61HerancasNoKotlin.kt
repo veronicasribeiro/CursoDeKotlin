318fe98d74650986b4e34e7a325a6ed522909753
Verônica Ribeiro <127154698+veronicasribeiro@users.noreply.github.com>
2023-10-25 11:55:01 -0300

    Adicionado nível de acesso (protected)

--- src/main/kotlin/tiposDeClasses/aula61HerancasNoKotlin.kt
+++ src/main/kotlin/tiposDeClasses/aula61HerancasNoKotlin.kt
@@ -1,6 +1,10 @@
 package tiposDeClasses
 
 // Conceito intermediário do OOP
+// Nível de Acesso
+// protected var - Somente pai e filhos podem acessar
+// private var - Somente a classe atual pode acessar as suas propriedades
+// (nada) default - Qualquer um pode mudar essa var (que esteja no mesmo pacote)
 fun main() {
     val professorA = Professor("Kakashi", "Rua A")
     println(professorA.descricao())
@@ -13,11 +17,17 @@ fun main() {
 
 // open class - A classe tradicional NÃO permite que se tenha herança, para isso usamos o OPEN CLASS
 open class Pessoa(var nome: String, var endereco: String) {
-    fun descricao() = "Meu nome é $nome e meu endereço é $endereco"
+    // protected - variável protegida para acesso somente para pai e filhos
+    protected var acessoBiblioteca = false
+    fun descricao() = "Meu nome é $nome e meu endereço é $endereco e meu acesso a biblioteca é: $acessoBiblioteca"
 }
 
 // instanciar Pessoa no "Professor(nome: String, endereco: String)"
 class Professor(nome: String, endereco: String) : Pessoa(nome, endereco) {
+    // Declarar construtor inicial (Init) para acessar a variável "acessoBiblioteca"
+    init {
+        acessoBiblioteca = true
+    }
 
 }
 
