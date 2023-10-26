86902ef2673e7e141e23a00e5fe9d127cd35872b
Verônica Ribeiro <127154698+veronicasribeiro@users.noreply.github.com>
2023-10-26 16:28:49 -0300

    Utilizando fun concreta em classe abstrata

--- src/main/kotlin/tiposDeClasses/aula65ClassesAbstratas.kt
+++ src/main/kotlin/tiposDeClasses/aula65ClassesAbstratas.kt
@@ -3,25 +3,41 @@ package tiposDeClasses
 fun main() {
     val guitarra = GuitarraTeste("Guitarra A")
     guitarra.afinar()
+    guitarra.descricao()
 
     val flauta = FlautaTeste("Flauta X")
     flauta.afinar()
+    flauta.descricao()
 
     val violino = ViolinoTeste("Violino Y")
     violino.afinar()
+    violino.descricao()
 }
 
 // abstract class
 /* 1.2 - Declarado uma classe abstrata para que todas as classes filhas definam por si só o comportamento, mesmo que a
 assinatura seja a mesma
  */
-abstract class InstrumentoTeste(nome: String) {
+abstract class InstrumentoTeste(var nome: String) {
 
     // Não utiliza colchetes
     // abstract é usada antes do fun
     abstract fun afinar()
     // A assinatura da função é a mesma afinar() -> Abstrato
     // Só que o comportamento é diferente -> Concreto (Bloco de códigos)
+
+    /* A classe abstrata não precisa só definir comportamentos abstratos, ela também pode definir comportamentos
+    concretos
+     */
+
+    /*
+    Fun - compartilhada em todas as classes filhas
+    abstract fun - compartilhada em todas as classes filhas só que com um comportamento
+    prorpio
+     */
+    fun descricao() {
+        println("Esse é o intrumento: $nome")
+    }
 }
 
 abstract class InstrumentoDeCordas(nome: String, var quantCordas: Int) : InstrumentoTeste(nome) {
