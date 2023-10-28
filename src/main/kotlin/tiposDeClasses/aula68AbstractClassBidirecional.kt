aca863989f14211ad0ebcde93696b826b4a5032d
Verônica Ribeiro <127154698+veronicasribeiro@users.noreply.github.com>
2023-10-28 11:46:32 -0300

    Criando um código mais generico de fácil manuntenção utilizando os parâmetros da super class

--- src/main/kotlin/tiposDeClasses/aula68AbstractClassBidirecional.kt
+++ src/main/kotlin/tiposDeClasses/aula68AbstractClassBidirecional.kt
@@ -3,16 +3,22 @@ package tiposDeClasses
 fun main() {
     val flautista = Musico2("Nico Robin")
     val flauta = Flauta2("Flauta X")
-    flauta.tocar(flautista)
+    tocar(flauta,flautista)
     println("________________________\n")
 
     val guitarrista = Musico2("Roronoa Zoro ")
     val guitarra = Guitarra2("Guitarra Y", 6)
-    guitarra.tocar(guitarrista)
+    tocar(guitarra,guitarrista)
     println("________________________\n")
 
     val violinista = Musico2("Vinsmoke Sanji")
     val violino = Violino2("Violino Z", 4)
-    violino.tocar(violinista)
+    tocar(violino,violinista)
     println("________________________\n")
+}
+
+// Parâmetros de Super Class
+// Como é uma classe abstrata eu tenho acesso aos métodos dela
+fun tocar(instrumento:Instrumento2, musico: Musico2) {
+    instrumento.tocar(musico)
 }
