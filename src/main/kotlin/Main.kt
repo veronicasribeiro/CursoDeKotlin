2ebac13abcb50b0120e89b560f0b1ca5f6a58b93
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-06 16:22:09 -0300

    feat: adicionado variáveis do tipo inferido e do tipo convencional, use variáveis do tipo convencional para ter mais segurança no código, embora o Kotlin saiba detectar os tipos.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,12 @@
-/*
-    Enunciado:
-    Crie um programa em Kotlin que faça o seguinte:
-
-    Declare uma variável mutável (var) que pode armazenar qualquer valor.
-    Atribua um valor inicial à variável (pode ser um número, uma string, etc.).
-    Use a funcionalidade ::class para descobrir o tipo da variável.
-    Dependendo do tipo da variável:
-    Se for String, imprima o texto em letras maiúsculas.
-    Se for Int, multiplique o número por 2 e imprima o resultado.
-    Se for Double, mostre o número com duas casas decimais.
-    Se for qualquer outro tipo, imprima uma mensagem indicando que o tipo não é suportado.
-* */
-
 fun main() {
 
-    var x = "MARIA"
+    // Tipo Convencional
+    val price: Double = 59.90
 
-    if (x is String) {
-        println("A variável $x é do tipo: ${x::class}")
-        println("A variável formatada para maiúsculo é: ${x.uppercase()}")
-    } else if (x is Int) {
-        println("A variável $x é do tipo: ${x::class}")
-        println("O resultado da multiplicação $x X 2 é: ${x * 2}")
-    } else if (x is Double) {
-        println("A variável $x é do tipo: ${x::class}")
-        val xFormatted = String.format("%.2f", x)
-        println("Número formatado: $xFormatted")
-    } else {
-        println("Tipo não suportado")
-    }
+    // Tipo inferido
+    val name = "Zoe"
+
+    // Sempre utilizamos tipos para trabalharmos de maneira segura, para evitar cometer erros
 
 }
 
