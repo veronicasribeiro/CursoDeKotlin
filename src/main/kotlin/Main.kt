39fdc3e505ec9e511ecc5e3510e8e2f76da75192
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-06 16:38:14 -0300

    feat: Tipos primitivos e espaços na memória

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,16 @@
 fun main() {
 
-    // Tipo Convencional
-    val price: Double = 59.90
+    // TIPOS PRIMITIVOS
+        // ESPAÇOS DE MEMÓRIA
 
-    // Tipo inferido
-    val name = "Zoe"
+    val price: Double = 2.58 // 64-bit ponto flutuante (floating point)
+    val name: String = "Verônica"
+    val age: Int = 21 // 32-bit (99% do tempo)
+    val byte: Byte = 8 // 8-bit
+    val short: Short = 16 // 16-bit
+    val long: Long = 1985646261584 // 64-bit (CPF / CNPJ)
+    val float: Float = 5.32F // 32-bit
 
-    // Sempre utilizamos tipos para trabalharmos de maneira segura, para evitar cometer erros
 
 }
 
