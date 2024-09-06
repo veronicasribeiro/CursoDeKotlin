352f65656f1a20cf698c44975200ecf0e491a7d1
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-05 22:51:28 -0300

    feat: exercício para fixar a ideia de variáveis mutáveis e imutáveis.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,22 @@
-// main - A primeira função que será executada (o botão de play irá aparecer)
-    // entry point (ponto de entrada)
-fun main() {
-    // Variável imutável
-    val product = "Imac"
+/*
+    Crie um programa em Kotlin que faça o seguinte:
 
-    println(product)
+    Declare uma variável imutável (val) para armazenar o nome de uma pessoa.
+    Declare uma variável mutável (var) para armazenar a idade dessa pessoa.
+    Mostre o nome e a idade da pessoa na tela.
+    Depois, atualize a idade da pessoa com um novo valor e mostre novamente a idade na tela.
+* */
+
+fun main() {
+    val name = "Verônica"
+    var age = 23
+
+    println("nome: $name")
+    println("idade: $age")
+
+    age = 24
+
+    println("Nova idade...")
+    println("idade: $age")
 }
 
