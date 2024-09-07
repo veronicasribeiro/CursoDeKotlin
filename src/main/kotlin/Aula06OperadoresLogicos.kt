0a7d6f6249604d6d4820a348f2ad4ccd048283c0
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-06 23:10:48 -0300

    feat: operadores lógicos

---
+++ src/main/kotlin/Aula06OperadoresLogicos.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // + - / *
+    var idade: Int = 31
+    idade = idade - 10
+    println(idade)
+
+    val textoIdade: String = "31"
+    // Cocatenar
+    println("Minha idade: " + textoIdade)
+
+    var booleana: Boolean = false
+    // O que é falso vira verdadeiro e o que é verdadeiro vira falso
+    println(!booleana)
+}
