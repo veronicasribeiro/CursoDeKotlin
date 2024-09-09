351c5fcb15ec29eb44fc4288637c0386331e684c
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-09 17:16:30 -0300

    feat: minha tentativa de resolver exercício: apliquei o operador elvis diretamente no valor null e não na variável que pode receber null

--- src/main/kotlin/Aula12NullableEElvis.kt
+++ src/main/kotlin/Aula12NullableEElvis.kt
@@ -14,6 +14,17 @@ fun main() {
     val quantidadeDeCaracteres = endereco?.length ?: 0 // operador elvis - como se fosse o else
     println(quantidadeDeCaracteres)
 
+    /*
+    *   Exercício: Verificação de Nome de Usuário
 
+        Crie um programa que recebe um nome de usuário (do tipo String?), onde o valor
+        pode ser nulo. Se o nome for nulo ou estiver vazio, o programa deve definir um
+        valor padrão de "Usuário Desconhecido" e exibir uma mensagem de boas-vindas com
+        esse nome. Caso contrário, exiba o nome fornecido.
+    * */
+
+    val nome: String? = null ?: "Usuário Desconhecido"
+    val mensagem: String = "Boas vindas, $nome!"
+    println(mensagem)
 
 }
