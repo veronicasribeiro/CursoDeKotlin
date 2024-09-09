7819a62b6bd6b2b5c7dbb38b67a58ff4d5092967
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-09 17:30:40 -0300

    feat: funções e reutilização do código

---
+++ src/main/kotlin/Aula13FuncoesEReutilizaçãoDeCodigos.kt
@@ -0,0 +1,16 @@
+fun main() {
+    // functions - forma de reutilizar blocos de códigos para organização e manutenção
+
+    olaMundo() // call
+
+    // Uma série de códigos
+
+    olaMundo()
+}
+
+// fun
+// nome da função
+// Parâmetros
+fun olaMundo() /* declaração */ { // corpo (body)
+    println("Olá, Mundo!")
+}
