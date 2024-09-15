44037ebc68024a6a50a4806c043a0312b0bebb28
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-15 17:00:26 -0300

    feat: exercicio de length que vai além disso, tenho que estudar mais coisas (como coleções)

---
+++ src/main/kotlin/02ExecicioPropriedadeLength.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val frase = "Eu amo Kotlin!"
+                                    // split(delimitador que separa)
+    val fraseSeparadaEmPalavras = frase.split(" ")
+
+    println(fraseSeparadaEmPalavras)
+    // caso de coleção - ainda não estudei
+}
+
