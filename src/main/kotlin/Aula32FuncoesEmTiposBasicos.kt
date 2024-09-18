ac124b44ef9bf5e8f0dc02fe431d9a9742568ded
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-18 16:38:13 -0300

    feat: funções em tipos básicos, tudo é tratado como objeto no Kotlin

---
+++ src/main/kotlin/Aula32FuncoesEmTiposBasicos.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    // Aqui tudo é tratado como um objeto
+    val antigoInt: Int = 3
+                // metodo .plus - Somar algum número
+    val novoInt = antigoInt.plus(30)
+    println(novoInt)
+
+}
