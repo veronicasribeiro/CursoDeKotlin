faa3aed54fbd35bbcfa6aa6c213e9bae5216a60a
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-28 21:57:24 -0300

    feat: singleton = objeto único que não precisa ser instanciado como a class

--- src/main/kotlin/Aula38SingletonsObjetosUnicos.kt
+++ src/main/kotlin/Aula38SingletonsObjetosUnicos.kt
@@ -1,6 +1,11 @@
 fun main() {
     // OBJETOS UNICOS (Singleton) = Padrão único
 
+    // classe -> é o molde para criar "N" objetos
+    // objetos -> é o conjunto de propriedades e comportamentos de um contexto do seu sistema (botão, telaDeLogin...)
+    // object -> é um objeto também, só que ele é único, ou seja, não é igual a class que sai vários objetos
+    // (exemplo: Banco de dados, sessão de usuário, logs)
+
     // A nossa classe consegue criar "n" objetos que a gente quiser
     val usuario1 = UsuarioObjetos("Maria")
     val usuario2 = UsuarioObjetos("João")
@@ -26,7 +31,8 @@ class UsuarioObjetos(val nome: String) {
 
 }
 
-// Um único objeto vivo durante o codigo
+// Um único objeto vivo disponivel para o sistema inteiro
+    // podemos usar para agrupar tudo que é constante no nosso programa
 object BancoDeDados {
     var nome = "Desconhecido"
     // Se eu declarar que a variável será nula, eu tenho que botar "?"
