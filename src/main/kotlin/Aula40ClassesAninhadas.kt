5529b08a2d1123a5196293b322ea74946914b7d0
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-29 18:27:16 -0300

    feat: classes internas

--- src/main/kotlin/Aula40ClassesAninhadas.kt
+++ src/main/kotlin/Aula40ClassesAninhadas.kt
@@ -2,27 +2,32 @@ fun main() {
     // TIPOS DE CLASSES
 
     // 1. Classes aninhadas (Nasted Class)
-    // 2.
+    // 2. Classes internas (Inner Class) - Aqui existe uma hierarquia
     // 3.
 
-    val madona = Musico()
-    madona.descricao()
+    val madonna = Musico()
+    madonna.descricao()
 
-    // Passa o nome da classe pai e logo em seguida você pode acessar as classes associadas a elas
-    val guitarra = Musico.Guitarra()
+    // para acessar inner class eu preciso usar o objeto vivo (madonna.)
+    val guitarra = madonna.Guitarra()
+    guitarra.descricao()
+    madonna.estilo = "MPB"
     guitarra.descricao()
 
+    // Passa o nome da classe pai e logo em seguida você pode acessar as classes associadas a elas
     val saxofone = Musico.Saxofone()
     saxofone.descricao()
+
 }
 
 class Musico {
-    var estilo: String = "Rock"
+    var estilo: String = "Rock" // Propriedades de objetos
     fun descricao() = println("Meu estilo é $estilo")
 
-   class Guitarra {
+    // inner = agora você terá acesso a variável "estilo" (acessar propriedades de uma classe superior)
+   inner class Guitarra {
        var cordas = 6
-       fun descricao() = println("Tocando uma guitarra com $cordas cordas")
+       fun descricao() = println("Tocando uma guitarra com $cordas cordas, e o estilo de música é $estilo")
    }
 
     class Saxofone {
